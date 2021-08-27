a45d40a7d56e3708a5a272a46b3b9f5f829935ea
IshaanPrince <78795543+IshaanPrince@users.noreply.github.com>
2021-08-27 14:31:54 +0530

    Add files via upload

---
+++ The Pyshop!.py
@@ -0,0 +1,41 @@
+print("You have 100 rupees before entering the Pyshop.")
+print(" I am Python the shop-keeper! what do you want to buy?")
+Items_in_the_shop="Milk","Fizz","powder","Cadbury","Pen","Pencil","Book","Eraser","Slate","Torch"
+print(Items_in_the_shop)
+print("50 rupees for Milk,20 rupees for Fizz,101 rupees for Powder,300 rupees for Cadbury,10 rupees for Pen,49 rupees For Pencil,508 rupees for Book,99 rupees for Eraser,17 rupees for Slate,99 for Torch")
+Thing=input("What to buy:")
+while Thing=="milk":
+    print("you bought a packet of milk and drank it and enjoyed for the day!")
+    break
+while Thing=="fizz":
+    print("What a cheap thing!You drank it and bad-luck.You made fun of Python , for fun  and Python got upset.Python bit you!!!")
+    break 
+while Thing=="powder":
+    print("You got a punch from Python for not having enough money instead of having powder!!!")
+    break
+while Thing=="cadbury":
+    print("You got a punch from Python for not having enough money instead of having cadbury!!cadbury became a sadbury!")
+    break
+while Thing=="pen":
+    print("Your teacher gave you full mark for the exam good handwriting!")
+    break
+while Thing=="pencil":
+    print("Low-Quality!it broke when you reached Home!")
+    break
+while Thing=="book":
+    print("You liked the story of the book very much!")
+    break
+while Thing=="eraser":
+    print("Good Eraser...")
+    break
+while Thing=="slate":
+    print("you rated slate disgusting!")
+    break
+while Thing=="torch":
+    print("torch is not working ! you got sad!")
+    break
+
+print("The end")
+
+
+
